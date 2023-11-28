a56e5f663cc8da28517560c815bd9c83c8162013
Sam Ball <stmball@gmail.com>
2023-11-28 15:48:30 +0000

    Font

--- gatsby-config.js
+++ gatsby-config.js
@@ -18,5 +18,17 @@ module.exports = {
         "path": "./src/images/"
       },
       __key: "images"
-    }]
+    },
+    {
+      resolve: 'gatsby-omni-font-loader',
+      options: {
+        "enableListener": true,
+        "preconnect": [`https://fonts.googleapis.com`, `https://fonts.gstatic.com`],
+        "web": [{
+          "name": "Victor Mono",
+          "file": "https://fonts.googleapis.com/css2?family=Victor+Mono:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;1,100;1,200;1,300;1,400;1,500;1,600;1,700&display=swap"
+        }]
+      }
+    }
+  ]
 };
